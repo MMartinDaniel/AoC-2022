01c9b44004011b2263f9ccf47e027e4d46b3e382
Daniel Martin Martinez <daniel.martinez@klarna.com>
2022-12-05 16:51:26 +0100

    wip

--- day5/gold/index.js
+++ day5/gold/index.js
@@ -1,28 +1,65 @@
-const {readFileSync } = require('fs');
+const { readFileSync } = require("fs");
 
-const content = readFileSync("./input.txt", 'utf-8');
+const content = readFileSync("./input.txt", "utf-8");
 
-let totalAmount = 0;
-const compare = (short,long) => short[0] >= long[0] || short[1] <= long[1] 
+const parseLine = (splittedLine) => {
+  const outputValues = [];
+  let currentLength = 0;
 
-content.split(/\r?\n/).forEach((line) => {
-  const [firstPair, secondPair] = line.split(",").map((item) => { 
-    const i = item.split("-")
-    return [parseInt(i[0]), parseInt(i[1])] 
-  });
+  for (let index = 0; index < splittedLine.length; index++) {
+    const element = splittedLine[index];
 
-  if(firstPair[1] < secondPair[0] || secondPair[1] < firstPair[0]) return
-  
-  
-  const firstPairLength = firstPair[1] - firstPair[0];
-  const secondPairLength = secondPair[1] - secondPair[0];
-  
-  if( firstPairLength > secondPairLength) {
-    if(compare(secondPair, firstPair)) totalAmount++
-  } else {
-    if(compare(firstPair, secondPair)) totalAmount++;    
+    if (element === "" && currentLength + 1 === 4) {
+      outputValues.push("_");
+      currentLength = 0;
+    } else if (element === "") currentLength++;
+    else {
+      outputValues.push(element[1]);
+      currentLength = 0;
+    }
   }
 
-})
+  return outputValues;
+};
 
-console.log(totalAmount)
+const fillStacks = () => {
+  stackData = stackData.slice(0, -2);
+  for (let index = stackData.length - 1; index >= 0; index--) {
+    const elementArray = stackData[index];
+    for (let elIndex = 0; elIndex < elementArray.length; elIndex++) {
+      if (!stacks[elIndex]) stacks[elIndex] = [];
+      if (elementArray[elIndex] !== "_")
+        stacks[elIndex].push(elementArray[elIndex]);
+    }
+  }
+  stacks.unshift([]);
+};
+
+const stacks = [];
+let stackData = [];
+let dataLoaded = false;
+
+content.split(/\r?\n/).forEach((line) => {
+  const splitLine = line.split(" ");
+
+  if (!splitLine[0].startsWith("move")) {
+    const stackLine = parseLine(splitLine);
+    stackData.push(stackLine);
+  } else {
+    if (!dataLoaded) {
+      fillStacks();
+      dataLoaded = true;
+    }
+    const [_1, amount, _2, from, _3, to] = line.split(" ");
+    const pile = stacks[from].slice(-amount);
+    stacks[from] = stacks[from].slice(0, -amount);
+    stacks[to] = stacks[to].concat(pile);
+  }
+});
+
+const solution = stacks.reduce(
+  (acc, cur) => (cur.length !== 0 ? `${acc}${cur.pop()}` : acc),
+  ""
+);
+
+console.log(solution);
--- day5/silver/index.js
+++ day5/silver/index.js
@@ -1,15 +1,66 @@
-const {readFileSync } = require('fs');
+const { readFileSync } = require("fs");
 
+const content = readFileSync("./example.txt", "utf-8");
 
-const stacks =  [[],["Z","N"],["M","C","D"],["P"]]
-//NOTE: Modified input
-const content = readFileSync("./example.txt", 'utf-8');
+const parseLine = (splittedLine) => {
+  const outputValues = [];
+  let currentLength = 0;
 
+  for (let index = 0; index < splittedLine.length; index++) {
+    const element = splittedLine[index];
+
+    if (element === "" && currentLength + 1 === 4) {
+      outputValues.push("_");
+      currentLength = 0;
+    } else if (element === "") currentLength++;
+    else {
+      outputValues.push(element[1]);
+      currentLength = 0;
+    }
+  }
+
+  return outputValues;
+};
+
+const fillStacks = () => {
+  stackData = stackData.slice(0, -2);
+  for (let index = stackData.length - 1; index >= 0; index--) {
+    const elementArray = stackData[index];
+    for (let elIndex = 0; elIndex < elementArray.length; elIndex++) {
+      if (!stacks[elIndex]) stacks[elIndex] = [];
+      if (elementArray[elIndex] !== "_")
+        stacks[elIndex].push(elementArray[elIndex]);
+    }
+  }
+  stacks.unshift([]);
+};
+
+const stacks = [];
+let stackData = [];
+let dataLoaded = false;
 
 content.split(/\r?\n/).forEach((line) => {
-  const [_1, amount,_2, from, _3 , to] = line.split(" ")
-  for(let i = 0; i < amount; i++) {
-      let value = stacks[from].pop()
-      stacks[to].push(value) 
+  const splitLine = line.split(" ");
+
+  if (!splitLine[0].startsWith("move")) {
+    const stackLine = parseLine(splitLine);
+    stackData.push(stackLine);
+  } else {
+    if (!dataLoaded) {
+      fillStacks();
+      dataLoaded = true;
+    }
+    const [_1, amount, _2, from, _3, to] = line.split(" ");
+    for (let i = 0; i < amount; i++) {
+      let value = stacks[from].pop();
+      stacks[to].push(value);
+    }
   }
-})
+});
+
+const solution = stacks.reduce(
+  (acc, cur) => (cur.length !== 0 ? `${acc}${cur.pop()}` : acc),
+  ""
+);
+
+console.log(solution);

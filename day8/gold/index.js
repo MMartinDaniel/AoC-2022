6e413c72ab7740923c8870e390ab63e49731669c
MMartin <danmartin.mez@gmail.com>
2022-12-08 19:17:54 +0100

    wip

--- day8/gold/index.js
+++ day8/gold/index.js
@@ -1,6 +1,65 @@
 const { readFileSync } = require("fs");
 
-const content = readFileSync("./example.txt", "utf-8");
+const content = readFileSync("./input.txt", "utf-8");
 
-content.split(/\r?\n/).forEach((line) => {
+const forest = content
+    .split(/\r?\n/)
+
+    .reduce((acc, line,i) => {
+
+        const input = line.split("")
+        const row = [...input.map(i => parseInt(i))]
+        
+        if(acc.columns.length === 0) acc.columns = Array.from({length: row.length}).fill([])
+        
+        // store reverse
+        acc.columns = row.reduce((acc,cur,j) => {
+            acc[j] = acc[j].concat([cur])
+            return acc
+        }, acc.columns)
+
+        return {
+            rows: [...acc.rows, row ],
+            columns: acc.columns,
+            rowsLength: row.length,
+            columnsLength: acc.columns.length
+        }
+    }
+,{ rows: [], columns: [], rowsLength: 0, columnsLength: 0})
+
+
+let highestScenicScore = 0
+
+const calculateScenic = (rowsOrColumns ,height, i, j) => {
+    const row = forest[rowsOrColumns][i];
+
+    let blocked = false
+
+    const countScenic = (acc,cur) => {
+        if(blocked) return acc
+        if(cur < height) return acc + 1
+        else {
+            blocked = true
+            return acc +  1
+        }
+    }
+ 
+    const leftScenic = row.slice(0,j).reverse().reduce(countScenic,0)
+    blocked = false;
+    
+    const rightScenic = row.slice(j+1,forest.rowsLength).reduce(countScenic,0)
+    return leftScenic * rightScenic
 };
+
+ for (let i = 1; i < forest.rowsLength -1; i++) {
+    const rowItem = forest.rows[i];
+    for (let j = 1; j < forest.columnsLength -1; j++) {
+        const threeHeight = rowItem[j];
+        const rowScenic = calculateScenic("rows",threeHeight,i,j)
+        const columnScenic = calculateScenic("columns",threeHeight,j,i)
+        
+        const totalScenic = rowScenic * columnScenic
+        if(totalScenic > highestScenicScore) highestScenicScore = totalScenic
+    }
+}
+console.log(highestScenicScore)
--- day8/silver/index.js
+++ day8/silver/index.js
@@ -1,6 +1,53 @@
 const { readFileSync } = require("fs");
 
-const content = readFileSync("./example.txt", "utf-8");
+const content = readFileSync("./input.txt", "utf-8");
 
-content.split(/\r?\n/).forEach((line) => {
+const forest = content
+    .split(/\r?\n/)
+
+    .reduce((acc, line,i) => {
+
+        const input = line.split("")
+        const row = [...input.map(i => parseInt(i))]
+        
+        if(acc.columns.length === 0) acc.columns = Array.from({length: row.length}).fill([])
+        
+        // store reverse
+        acc.columns = row.reduce((acc,cur,j) => {
+            acc[j] = acc[j].concat([cur])
+            return acc
+        }, acc.columns)
+
+        return {
+            rows: [...acc.rows, row ],
+            columns: acc.columns,
+            rowsLength: row.length,
+            columnsLength: acc.columns.length
+        }
+    }
+,{ rows: [], columns: [], rowsLength: 0, columnsLength: 0})
+
+let amountOfTreesVisible = (forest.rowsLength - 2) * 2 + forest.rowsLength * 2
+
+
+const isTreeVisible = (rowsOrColumns ,height, i, j) => {
+    const row = forest[rowsOrColumns][i];
+    
+    const visibleFromLeft = !row.slice(0,j).some(tree => tree >= height)
+    const visibleFromRight = !row.slice(j+1,forest.rowsLength).some(tree => tree >= height)
+
+    return visibleFromLeft || visibleFromRight;
 };
+
+ for (let i = 1; i < forest.rowsLength -1; i++) {
+    const rowItem = forest.rows[i];
+    for (let j = 1; j < forest.columnsLength -1; j++) {
+        const threeHeight = rowItem[j];
+         if (
+            isTreeVisible("rows",threeHeight,i,j) ||
+            isTreeVisible("columns",threeHeight,j,i)
+        ) amountOfTreesVisible++  
+    }
+}
+
+console.log(amountOfTreesVisible)

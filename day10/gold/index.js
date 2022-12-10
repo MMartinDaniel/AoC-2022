f7d320f76632542b3431297a5d47354240d22189
Daniel Martin Martinez <daniel.martinez@klarna.com>
2022-12-10 14:16:22 +0100

    wip

--- day10/gold/index.js
+++ day10/gold/index.js
@@ -2,77 +2,61 @@ const { readFileSync } = require("fs");
 
 const content = readFileSync("./input.txt", "utf-8");
 
-
 let registerX = 1;
-
-const timeToCompleteInstruction = {
-    "addx": 2,
-    "noop": 1
-}
-
-
-const CRT_WIDTH = 40
-const CRT_HEIGHT = 6
-
-const  crt = []
-for (let i = 0; i < CRT_HEIGHT; i++) {
-    crt[i] = []
-    for (let j = 0; j < CRT_WIDTH ; j++) {
-        crt[i][j] = "."
-        
-    }
-}
-
-const printCrt = () => {
-    for (let i = 0; i < CRT_HEIGHT; i++) {
-       console.log(crt[i].join(""))
-    }     
-}
-
-
 let cicles = 0;
 let currentPosition = -1;
 
+const CRT_WIDTH = 40;
+const CRT_HEIGHT = 6;
+const crt = [];
+const spritePosition = [];
 
-const printCrtRow = (CurrentCicle, currentPosition) => {
+const timeToCompleteInstruction = {
+  addx: 2,
+  noop: 1,
+};
 
-    for (let i = 0; i < 10; i++) 
-        if(CurrentCicle <= i * 20 * 2) {
-            if(spritePosition[currentPosition] === "#") crt[i-1][currentPosition] = "#"
-            return crt[i-1].slice(0, currentPosition+1).join("") 
-        }
+// fill CRT
+for (let i = 0; i < CRT_HEIGHT; i++) {
+  crt[i] = [];
+  for (let j = 0; j < CRT_WIDTH; j++) {
+    crt[i][j] = ".";
+  }
 }
 
-const spritePosition = []
+const printOnCrtRow = (currentCicle, currentPosition) => {
+  for (let i = 0; i < 10; i++)
+    if (currentCicle <= i * 20 * 2) {
+      if (spritePosition[currentPosition] === "#")
+        crt[i - 1][currentPosition] = "#";
+      return;
+    }
+};
 
 const drawSprite = () => {
-    for (let i = 0; i < CRT_WIDTH; i++) {
-        spritePosition[i] = (i === registerX || i === registerX -1 || i === registerX +1) ? "#" : "."
-    }
-    return spritePosition.join("")
-}
-console.log(drawSprite())
+  for (let i = 0; i < CRT_WIDTH; i++) {
+    spritePosition[i] =
+      i === registerX || i === registerX - 1 || i === registerX + 1 ? "#" : ".";
+  }
+};
+
+// draw initial sprite
+drawSprite();
+
 content.split(/\r?\n/).forEach((line) => {
-    const [instruction, value] = line.split(" ")
+  const [instruction, value] = line.split(" ");
 
-    for (let i = 0; i < timeToCompleteInstruction[instruction]; i++) {
-        cicles++
-        currentPosition++
-        console.log(`Start Cicle ${cicles}: being executing ${instruction} ${value}`)
-        console.log(`During cycle  ${cicles}: CRT draws pixel in position ${currentPosition}`)
-        console.log(`Current CRT row: ${printCrtRow(cicles, currentPosition)}`)
-        if(currentPosition === 39) currentPosition = -1;
+  for (let i = 0; i < timeToCompleteInstruction[instruction]; i++) {
+    cicles++;
+    currentPosition++;
+    printOnCrtRow(cicles, currentPosition);
+    if (currentPosition === 39) currentPosition = -1;
+  }
+  if (value) {
+    registerX += parseInt(value);
+    drawSprite();
+  }
+});
 
-         console.log("             ")
-    }
-    if(value) { 
-        registerX += parseInt(value)
-        console.log(`sprite Position: ${drawSprite()}`)
-    }
-
-    console.log(`End of Cicle ${cicles}, value of Register X = ${registerX}`)
-    console.log("             ")
-
-})
-
-printCrt()
+// PRINT the screen
+crt.map((i) => console.log(i.join("")));
--- day10/silver/index.js
+++ day10/silver/index.js
@@ -2,32 +2,35 @@ const { readFileSync } = require("fs");
 
 const content = readFileSync("./input.txt", "utf-8");
 
-
 let registerX = 1;
+let cicles = 0;
 
 const timeToCompleteInstruction = {
-    "addx": 2,
-    "noop": 1
-}
+  addx: 2,
+  noop: 1,
+};
 
 const signalStrengthAtCicle = {
-    20: 0,
-    60: 0,
-    100: 0,
-    140: 0,
-    180: 0,
-    220: 0
-}
+  20: 0,
+  60: 0,
+  100: 0,
+  140: 0,
+  180: 0,
+  220: 0,
+};
 
-let cicles = 0;
 content.split(/\r?\n/).forEach((line) => {
-    const [instruction, value] = line.split(" ")    
-    for (let i = 0; i < timeToCompleteInstruction[instruction]; i++) {
-        cicles++
-        if(signalStrengthAtCicle[cicles] >= 0) signalStrengthAtCicle[cicles] = cicles * registerX
-    }
-    if(value) registerX += parseInt(value)
-})
+  const [instruction, value] = line.split(" ");
+  for (let i = 0; i < timeToCompleteInstruction[instruction]; i++) {
+    cicles++;
+    if (signalStrengthAtCicle[cicles] >= 0)
+      signalStrengthAtCicle[cicles] = cicles * registerX;
+  }
+  if (value) registerX += parseInt(value);
+});
 
-const totalSignalStrength = Object.values(signalStrengthAtCicle).reduce((acc,cur) => acc + cur, 0)
-console.log(totalSignalStrength)
+const totalSignalStrength = Object.values(signalStrengthAtCicle).reduce(
+  (acc, cur) => acc + cur,
+  0
+);
+console.log(totalSignalStrength);
